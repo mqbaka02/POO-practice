7ed73032867199966074d4b2bfffad740f4eaff7
Ratsimandresy Michael <mqbaka@gmail.com>
2024-09-12 11:45:51 +0300

    Solved the problem.

--- config/config.php
+++ config/config.php
@@ -1,18 +1,9 @@
 <?php
-
 use Framework\Renderer\RendererInterface;
-// use Framework\Renderer\TwigRenderer;
 use Framework\Renderer\TwigRendererFactory;
-// use Psr\Container\ContainerInterface;
-
-// use function DI\create;
 use function DI\factory;
 
 return [
     'views.path'=> dirname(__DIR__) . '/views',
-    // RendererInterface::class => function (ContainerInterface $container) {
-    //     return new TwigRenderer($container->get('config.view_path'));
-    // }
-    // RendererInterface::class => create(TwigRenderer::class)->constructor(\DI\get('config.view_path'))
     RendererInterface::class => factory(TwigRendererFactory::class)
 ];
